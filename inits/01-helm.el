61a89e92578e12f94288b0c03fab41f7ede2050e
sekiya <sekiya.takeshi231@canon.co.jp>
2013-08-29 09:22:04 +0900

    Added helm resume.

--- inits/01-helm.el
+++ inits/01-helm.el
@@ -2,3 +2,4 @@
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-M-z") 'helm-resume)
