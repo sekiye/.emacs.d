05fd180e4a2daa53cd9baa77466430931dbe78ce
sekiya <sekiya.takeshi231@canon.co.jp>
2013-08-29 14:04:55 +0900

    Added goto-chg, auto-save-buffers and undo-tree.

---
+++ elpa/goto-chg-20080919.2256/goto-chg-pkg.el
@@ -0,0 +1,1 @@
+(define-package "goto-chg" "20080919.2256" "goto last change [wiki]" (quote nil))
---
+++ inits/11-goto-chg.el
@@ -0,0 +1,4 @@
+(require 'goto-chg)
+
+(global-set-key "\C-xj" 'goto-last-change)
+(global-set-key "\C-xJ" 'goto-last-change-reverse)
