c935a3dae1da3eba9bb2c7b18a37bb7fd66d11c7
sekiya <sekiya.takeshi231@canon.co.jp>
2014-01-06 16:05:56 +0900

    Added helm ls git.

--- inits/01-helm.el
+++ inits/01-helm.el
@@ -1,4 +1,5 @@
 (require 'helm-config)
+(require 'helm-ls-git)
 (global-set-key (kbd "C-x C-f") 'helm-for-files)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
@@ -8,3 +9,12 @@
 
 (global-set-key (kbd "C-M-o") 'helm-occur)
 (define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
+
+(setq helm-for-files-preferred-list
+      '(helm-source-buffers-list
+        helm-source-recentf
+        helm-source-bookmarks
+        helm-source-file-cache
+        helm-source-files-in-current-dir
+        helm-source-locate
+        helm-source-ls-git))
