92ee8f9b8cfa2b3994698ae8c1b8c0e57e18c2e2
sekiya <sekiya.takeshi231@canon.co.jp>
2015-04-23 15:11:32 +0900

    Fix for linux.

--- inits/20-moccur.el
+++ inits/20-moccur.el
@@ -1,5 +1,6 @@
 (require 'color-moccur)
 (require 'moccur-edit)
+(require 'bind-key)
 (setq moccur-split-word t)
 (setq dmoccur-recursive-search t)
-(bind-key "M-O" 'isearch-moccur-all isearch-mode-map))
+(bind-key "M-O" 'isearch-moccur-all isearch-mode-map)
--- inits/linux-flycheck.el
+++ inits/linux-flycheck.el
@@ -1,4 +1,4 @@
-(setq flycheck-clang-include-path (list "." "../../include" "include" "/usr/local/include/compute" "/usr/local/cuda/include"))
+(setq flycheck-clang-include-path (list "." "../../include" "include" "/usr/local/include/compute" "/usr/local/cuda/include" "/home/sekiya/src/cpputest/include" "/opt/AMDAPPSDK-3.0-0-Beta/include"))
 (add-hook 'c++-mode-hook (lambda () (setq flycheck-clang-language-standard "c++11")))
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
