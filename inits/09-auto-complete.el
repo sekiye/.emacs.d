4d92dd946dde135bdd6a262f3288bac869d07be8
sekiye <sekiye@gmail.com>
2015-11-22 22:55:08 +0900

    Fix

--- inits/09-auto-complete.el
+++ inits/09-auto-complete.el
@@ -1,6 +1,6 @@
-(require 'auto-complete-config)
-(ac-config-default)
+;; (require 'auto-complete-config)
+;; (ac-config-default)
 
-(require 'ac-helm)
-(global-set-key (kbd "C-o") 'ac-complete-with-helm)
-(define-key ac-complete-mode-map (kbd "C-o") 'ac-complete-with-helm)
+;; (require 'ac-helm)
+;; (global-set-key (kbd "C-o") 'ac-complete-with-helm)
+;; (define-key ac-complete-mode-map (kbd "C-o") 'ac-complete-with-helm)
---
+++ inits/28-company-irony.el
@@ -0,0 +1,18 @@
+(require 'company)
+(global-company-mode) ; 全バッファで有効にする 
+(setq company-idle-delay 0) ; デフォルトは0.5
+(setq company-minimum-prefix-length 2) ; デフォルトは4
+(setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
+(require 'irony)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-to-list 'company-backends 'company-irony) ; backend追加
+(setq irony-lang-compile-option-alist
+      '((c++-mode . ("c++" "-std=c++11" "-lstdc++" "-lm"))
+        (c-mode . ("c"))
+        (objc-mode . '("objective-c"))))
+(defun irony--lang-compile-option ()
+  (irony--awhen (cdr-safe (assq major-mode irony-lang-compile-option-alist))
+    (append '("-x") it)))
